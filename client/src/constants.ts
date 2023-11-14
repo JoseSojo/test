201bd4f777b7de8a224f29b2b8d8e67606461d90
JoseSojo <jsojo346@gmail.com>
2023-11-14 12:39:21 -0400

    add url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
 
 export const NAVIGATION_EVENT = 'pushstate';
-//export const BASIC_URL = 'https://api-filter-url.onrender.com';
-export const BASIC_URL = 'http://localhost:7654';
+export const BASIC_URL = 'https://api-filter-url.onrender.com';
+//export const BASIC_URL = 'http://localhost:7654';
